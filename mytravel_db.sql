f3c5eb6d679ed13421455dfb2495640d1a7a1673
Tarasato <taramiratsu@gmail.com>
2025-02-05 14:23:04 +0700

    V.1.1.2

--- mytravel_db.sql
+++ mytravel_db.sql
@@ -33,7 +33,7 @@ CREATE TABLE `travel_tb` (
   PRIMARY KEY (`travelId`),
   KEY `travellerId` (`travellerId`),
   CONSTRAINT `travel_tb_ibfk_1` FOREIGN KEY (`travellerId`) REFERENCES `traveller_tb` (`travellerId`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `travel_tb` (
 --
 
 /*!40000 ALTER TABLE `travel_tb` DISABLE KEYS */;
-INSERT INTO `travel_tb` VALUES (1,'กรุงเทพมหานคร','2025-01-10','2025-01-12',4500,1,''),(2,'เชียงใหม่','2025-02-01','2025-02-05',7500,2,''),(3,'ภูเก็ต','2025-03-15','2025-03-18',12000,3,''),(4,'กระบี่','2025-04-01','2025-04-04',9500,1,''),(5,'สุโขทัย','2025-05-10','2025-05-12',3800,2,''),(6,'อยุธยา','2025-06-20','2025-06-21',1500,3,''),(7,'พัทยา','2025-07-05','2025-07-07',3200,1,''),(8,'หัวหิน','2025-08-12','2025-08-15',4700,2,''),(9,'เขาใหญ่','2025-09-18','2025-09-20',5500,3,''),(10,'เกาะสมุย','2025-10-01','2025-10-05',15500,1,''),(11,'เกาะเสม็ด','2025-11-10','2025-11-12',6300,2,''),(12,'น่าน','2025-12-01','2025-12-04',7200,3,''),(13,'แม่ฮ่องสอน','2026-01-15','2026-01-20',10200,1,''),(15,'Taramiratsu\'s home','2025-01-10','2025-01-10',200,5,'travel_1236081442963.png');
+INSERT INTO `travel_tb` VALUES (1,'กรุงเทพมหานคร','2025-01-10','2025-01-12',4500,1,''),(2,'เชียงใหม่','2025-02-01','2025-02-05',7500,2,''),(3,'ภูเก็ต','2025-03-15','2025-03-18',12000,3,''),(5,'ม.เอเชีย','2025-02-05','2025-02-05',200,5,''),(6,'อยุธยา','2025-06-20','2025-06-21',1500,3,''),(7,'พัทยา','2025-07-05','2025-07-07',3200,1,''),(8,'หัวหิน','2025-08-12','2025-08-15',4700,2,''),(9,'เขาใหญ่','2025-09-18','2025-09-20',5500,3,''),(10,'เกาะสมุย','2025-10-01','2025-10-05',15500,1,''),(11,'เกาะเสม็ด','2025-11-10','2025-11-12',6300,2,''),(12,'น่าน','2025-12-01','2025-12-04',7200,3,''),(13,'แม่ฮ่องสอน','2026-01-15','2026-01-20',10200,1,''),(15,'Taramiratsu\'s home','2025-01-10','2025-01-10',200,5,'travel_1236081442963.png');
 /*!40000 ALTER TABLE `travel_tb` ENABLE KEYS */;
 
 --
@@ -58,7 +58,7 @@ CREATE TABLE `traveller_tb` (
   `travellerPassword` varchar(50) NOT NULL,
   `travellerImage` varchar(150) NOT NULL,
   PRIMARY KEY (`travellerId`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +66,7 @@ CREATE TABLE `traveller_tb` (
 --
 
 /*!40000 ALTER TABLE `traveller_tb` DISABLE KEYS */;
-INSERT INTO `traveller_tb` VALUES (1,'สมชาย ใจดี','somchai.jai@gmail.com','password123',''),(2,'สมหญิง สง่า','somying.sa@gmail.com','pass456',''),(3,'อนุชา หนุ่มนิรันดร์','anucha.nu@gmail.com','secure789',''),(4,'Tarasato','taramiratsu@gmail.com','123456',''),(5,'Taramiratsu Xato','taramiratsuxato@gmail.com','123456','traveller_1286337387965.png');
+INSERT INTO `traveller_tb` VALUES (1,'สมชาย ใจใหญ่','somchai.jai@gmail.com','password123','traveller_890297991328.png'),(2,'สมหญิง สง่า','somying.sa@gmail.com','pass456',''),(3,'อนุชา หนุ่มนิรันดร์','anucha.nu@gmail.com','secure789',''),(4,'Tarasato','taramiratsu@gmail.com','123456',''),(5,'Taramiratsu Xato','taramiratsuxato@gmail.com','123456','traveller_1286337387965.png');
 /*!40000 ALTER TABLE `traveller_tb` ENABLE KEYS */;
 
 --
@@ -82,4 +82,4 @@ INSERT INTO `traveller_tb` VALUES (1,'สมชาย ใจดี','somchai.jai
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-22 14:59:31
+-- Dump completed on 2025-02-05 14:22:53
