49a9808a09ba2af2bbda46c9c791258c92616702
Tarasato <taramiratsu.xato@gmail.com>
2025-03-03 12:59:49 +0700

    Update DB

--- mytravel_db.sql
+++ mytravel_db.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `travel_tb`
 --
-
+USE travel_db;
 DROP TABLE IF EXISTS `travel_tb`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
